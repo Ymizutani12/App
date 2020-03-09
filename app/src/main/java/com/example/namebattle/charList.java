e2dbafef6169716d37ec33ec8f14b40d38bddc78
Ymizu <y_mizutani@freestyles.jp>
2020-03-09 20:50:02 +0900

    2020/03/09 1h "メイン画面"

---
+++ app/src/main/java/com/example/namebattle/charList.java
@@ -0,0 +1,14 @@
+package com.example.namebattle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class charList extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_char_list);
+    }
+}
