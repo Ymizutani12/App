7fc285b6bbb422b3b5f7486f18e57f08196c06f4
Ymizu <y_mizutani@freestyles.jp>
2020-03-10 21:33:49 +0900

    2020/03/10 1h "キャラ一覧画面"

--- app/src/main/java/com/example/namebattle/MainActivity.java
+++ app/src/main/java/com/example/namebattle/MainActivity.java
@@ -2,7 +2,10 @@ package com.example.namebattle;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +13,17 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Button CharButton = findViewById(R.id.charbutton);
+        CharButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(getApplication(), charList.class);
+                startActivity(intent);
+
+            }
+
+        });
+
     }
 }
